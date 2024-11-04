0b94f5c65e8a0b3f1d72edeb8c25f32b9c88e612
maxxxkam <muslimovmaxkam@gmail.com>
2024-11-04 15:14:40 +0500

    Add files via upload

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,24 +1,38 @@
-import React from 'react'
-import './Header.scss'
+import React, { useState } from 'react';
+import './Header.scss';
 
 const Header = () => {
-  return (
-    <>
-    <header className="header">
-        <div className="container">
-            <div className="header__wrap">
-                <h1 className="logo">Digital Agency</h1>
-               <div className="header__a">
-               <a href="">home</a>
-                <a href="">about</a>
-                <a href="">testimonials</a>
-                <a href="">contact</a>
-               </div>
-            </div>
-        </div>
-    </header>
-    </>
-  )
-}
+    const [burger, setBurger] = useState(false);
+    const toggleBurger = () => {
+        setBurger(!burger);
+    };
 
-export default Header
+    return (
+        <>
+            <header className="header">
+                <div className="container">
+                    <div className="header__wrapper">
+                    <h1 className="logo">Digital Agency</h1>
+                    <div className={`header__content ${burger ? 'burger-active' : ''}`}>
+                        <div className="header__links">
+                            <a href="#">home</a>
+                            <a href="#">about</a>
+                            <a href="#">testimonials</a>
+                            <a href="#">contact</a>
+                        </div>
+                       
+                    </div>
+                    <img 
+                            onClick={toggleBurger} 
+                            className={`burger ${burger ? 'open' : ''}`} 
+                            src="https://cdn-icons-png.flaticon.com/512/17213/17213363.png" 
+                            alt="Menu" 
+                        />
+                    </div>
+                </div>
+            </header>
+        </>
+    );
+};
+
+export default Header;
